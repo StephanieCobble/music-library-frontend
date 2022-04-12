e979eb8b4e1e14bd5a4e3a05b03f91ed05ce7c4f
StephanieCobble <cobble.stephanie@gmail.com>
2022-04-12 14:03:26 -0700

    revised the add song form so it will auto refresh after add

--- src/Components/AddSong/AddSong.jsx
+++ src/Components/AddSong/AddSong.jsx
@@ -1,41 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import MusicTable from "../MusicTable/MusicTable";
 
 const AddSong = (props) => {
+  const [songTitle, setSongTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [albumName, setAlbumName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [songs, setSongs] = useState([]);
 
-    const [songTitle, setSongTitle] = useState('');
-    const [artist, setArtist] = useState('');
-    const [albumName, setAlbumName] = useState('');
-    const [genre, setGenre] = useState('');
-    const [releaseDate, setReleaseDate] = useState('');
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        let newSong = {
-            songTitle: songTitle,
-            artist: artist,
-            albumName: albumName,
-            genre: genre,
-            releaseDate: releaseDate
-        };
-        console.log(newSong);
-        props.addNewSongProperty(newSong)
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newSong = {
+      title: songTitle,
+      artist: artist,
+      album: albumName,
+      genre: genre,
+      release_date: releaseDate,
+    };
+   setSongTitle("")
+   setArtist("")
+   setAlbumName("")
+   setGenre("")
+   setReleaseDate("")
+   AddNewSong(newSong)
+  }
 
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <label>Song Title</label>
-            <input type='string' value={songTitle} onChange={(event) => setSongTitle(event.target.value)} />
-            <label>Artist</label>
-            <input type='string' value={artist} onChange={(event) => setArtist(event.target.value)} />
-            <label>Album Name</label>
-            <input type='string' value={albumName} onChange={(event) => setAlbumName(event.target.value)} />
-            <label>Genre</label>
-            <input type='string' value={genre} onChange={(event) => setGenre(event.target.value)} />
-            <label>Release Date</label>
-            <input type='date' value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)} />
-            <button type='submit'>Add Song</button>
-        </form>
+  async function getAllSongs() {
+    let response = await axios.get("http://127.0.0.1:8000/api/music/");
+    setSongs(response.data);
+  }
 
-     );
-}
+  async function AddNewSong(newSong) {
+        let response = await axios.post("http://127.0.0.1:8000/api/music/", newSong);
+        if(response.status === 201) {
+            await getAllSongs();
+            window.location.reload()
+        }}
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song Title</label>
+      <input
+        type="string"
+        value={songTitle}
+        onChange={(event) => setSongTitle(event.target.value)}
+      />
+      <label>Artist</label>
+      <input
+        type="string"
+        value={artist}
+        onChange={(event) => setArtist(event.target.value)}
+      />
+      <label>Album Name</label>
+      <input
+        type="string"
+        value={albumName}
+        onChange={(event) => setAlbumName(event.target.value)}
+      />
+      <label>Genre</label>
+      <input
+        type="string"
+        value={genre}
+        onChange={(event) => setGenre(event.target.value)}
+      />
+      <label>Release Date</label>
+      <input
+        type="date"
+        value={releaseDate}
+        onChange={(event) => setReleaseDate(event.target.value)}
+      />
+      <button  type="submit">Add Song</button>
+    </form>
+  );
+};
 export default AddSong;
